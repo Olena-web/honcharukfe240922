f6eaa889d4aed981348c78d872d42a22770e181e
Olena Honcharuk <gasmin07@gmail.com>
2022-09-26 17:32:14 +0300

    feat: change style, add responsive

--- src/Chart/Chart.tsx
+++ src/Chart/Chart.tsx
@@ -1,17 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { getRandomNum } from '../helpers';
 
 import './Chart.css';
 
 export function Chart() {
-    let colorProgressBefore = 'lightgray';
-    let colorProgressAfter = ' rgb(107, 227, 207)';
+ 
+    let colorProgressBefore = 'rgb(236, 242, 245)';
+    let colorProgressAfter = ' rgb(184, 225, 219)';
     
     let backgroundColorInit = `linear-gradient(to right, ${colorProgressAfter}, ${colorProgressAfter}, 50%, ${colorProgressBefore} 75%, ${colorProgressBefore} 75%)`
-    // const style = {
-    //     background: linear-gradient(to right, ${colorProgressAfter}, ${colorProgressAfter}, 50%, ${colorProgressBefore} 75%, ${colorProgressBefore} 75%))
-    // }
-
+    
     const initState = {
         landingData: {
         name: "Landing Page",
@@ -38,20 +36,40 @@ export function Chart() {
       
       const [data, setData] = useState(initState);
          
-    const UseSubmitHandler = () => {
+    const submitHandler = () => {
       const landingTime = getRandomNum(0.1, 10.0);
       const configTime = getRandomNum(0.1, 10.0);
       const checkoutTime = getRandomNum(0.1, 10.0);
       const dealTime = getRandomNum(0.1, 10.0);
+      const total = landingTime + configTime + checkoutTime + dealTime;
+      const multiplier = 100 / total;
      
       
-      let landingImage = `linear-gradient(to right,  ${colorProgressAfter} ${landingTime}%, ${colorProgressBefore} ${landingTime}%, ${colorProgressBefore} 100%)`;
+      let landingImage = `linear-gradient(to right,  
+        ${colorProgressAfter} ${landingTime*multiplier}%, 
+        ${colorProgressBefore} ${landingTime*multiplier}%, 
+        ${colorProgressBefore} 100%)`;
 
-      let configImage = `linear-gradient(to right, ${colorProgressBefore} ${landingTime}%, ${colorProgressAfter} ${landingTime}%, ${colorProgressAfter} ${landingTime + configTime}%, ${colorProgressBefore} ${landingTime + configTime}%, ${colorProgressBefore} 100%)`;
+      let configImage = `linear-gradient(to right, 
+        ${colorProgressBefore} ${landingTime*multiplier}%, 
+        ${colorProgressAfter} ${landingTime*multiplier}%, 
+        ${colorProgressAfter} ${landingTime*multiplier + configTime*multiplier}%, 
+        ${colorProgressBefore} ${landingTime*multiplier + configTime*multiplier}%, 
+        ${colorProgressBefore} 100%)`;
 
-      let checkoutImage = `linear-gradient(to right, ${colorProgressBefore} ${landingTime + configTime}%, ${colorProgressAfter} ${landingTime+ configTime}%, ${colorProgressAfter} ${landingTime + configTime + checkoutTime}%, ${colorProgressBefore} ${landingTime + configTime + checkoutTime}%, ${colorProgressBefore} 100%)`;
+      let checkoutImage = `linear-gradient(to right, 
+        ${colorProgressBefore} ${(landingTime + configTime)*multiplier}%, 
+        ${colorProgressAfter} ${(landingTime+ configTime)*multiplier}%, 
+        ${colorProgressAfter} ${(landingTime + configTime + checkoutTime)*multiplier}%, 
+        ${colorProgressBefore} ${(landingTime + configTime + checkoutTime)*multiplier}%, 
+        ${colorProgressBefore} 100%)`;
 
-      let dealImage = `linear-gradient(to right, ${colorProgressBefore} ${landingTime + configTime+checkoutTime}%, ${colorProgressAfter} ${landingTime+ configTime+checkoutTime}%, ${colorProgressAfter} ${landingTime + configTime + checkoutTime+ dealTime}%, ${colorProgressBefore} ${landingTime + configTime +checkoutTime+ dealTime}%, ${colorProgressBefore} 100%)`;
+      let dealImage = `linear-gradient(to right, 
+        ${colorProgressBefore} ${(landingTime + configTime+checkoutTime)*multiplier}%, 
+        ${colorProgressAfter} ${(landingTime+ configTime+checkoutTime)*multiplier}%, 
+        ${colorProgressAfter} ${(landingTime + configTime + checkoutTime+ dealTime)*multiplier}%, 
+        ${colorProgressBefore} ${(landingTime + configTime +checkoutTime+ dealTime)*multiplier}%, 
+        ${colorProgressBefore} 100%)`;
       
         setData({
             landingData: {
@@ -75,26 +93,14 @@ export function Chart() {
                 backgroundImage: dealImage,
             }
         });
-         
-      useEffect(() => {
-          UseSubmitHandler();
-        }, []);
-       
+     
       };
-      // const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      //   e.preventDefault();
-      //   console.log(e.target, e.target.value);      
-      //   setData({
-      //       ...data,
-      //       [e.target.name]: e.target.value
-      //   });
 
-        
-        
-      //   };
+     
+     
        
     return (
-        <div className="Chart">
+        <div className="chart">
         
             <div className='line-chart'>
             <input
@@ -102,7 +108,7 @@ export function Chart() {
                 name="landingData"
                 type="text"
                 value={data.landingData.name}
-                //onChange={onChange}
+                
             />
       <input
         className="time"
@@ -110,7 +116,7 @@ export function Chart() {
         type="text"
         value={data.landingData.time}
         style={{ backgroundImage: data.landingData.backgroundImage }}
-        //onChange={SubmitHandler}
+       
       />
             </div>
             <div className="line-chart">
@@ -119,7 +125,7 @@ export function Chart() {
         name="configData"
         type="text"
         value={data.configData.name}
-        //onChange={onChange}
+       
       />
       <input
         className="time"
@@ -127,7 +133,7 @@ export function Chart() {
         type="text"
         value={data.configData.time}
         style={{ backgroundImage: data.configData.backgroundImage}}
-        //onChange={onChange}
+        
       />
             </div>
             <div className="line-chart">
@@ -144,7 +150,7 @@ export function Chart() {
         type="text"
         value={data.checkoutData.time}
         style={{ backgroundImage: data.checkoutData.backgroundImage }}
-        //onChange={onChange}
+        
       />
                 
             </div>
@@ -162,7 +168,7 @@ export function Chart() {
         type="text"
         value={data.dealData.time}
         style={{ backgroundImage: data.dealData.backgroundImage }}
-       // onChange={onChange}
+       
       />
     </div>
     
@@ -173,7 +179,7 @@ export function Chart() {
       {/* Submit Button */}
       <button type="submit"
       className="submitButton"
-      onClick={UseSubmitHandler}
+      onClick={submitHandler}
       
       >Submit</button>
      
@@ -182,3 +188,7 @@ export function Chart() {
     </div>
     );
     }
+     setInterval(() => {
+        document.querySelector<HTMLButtonElement>('.submitButton')?.click();
+      
+      }, 31800);
